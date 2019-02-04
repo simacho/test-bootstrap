590e8776ecde296e7b6e86f5fb92d9aef8f7a9cb
simazaki katsuya <simazakikatuya@gmail.com>
2019-02-05 08:33:21 +0900

    change

---
+++ src/firebase/config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyCupuqcB7i6-b-32uo0iF4n6_SpPvHe_YY",
+    authDomain: "nowait-7209f.firebaseapp.com",
+    databaseURL: "https://nowait-7209f.firebaseio.com",
+    projectId: "nowait-7209f",
+    storageBucket: "nowait-7209f.appspot.com",
+    messagingSenderId: "1009447499857"
+};
+
