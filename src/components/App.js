13316d5e6c9b9dcf11ac259bc3e99f6bab98f806
Katsuya Shimazaki <simazakikatuya@gmail.com>
2019-06-18 09:08:25 +0900

    change

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,5 @@
 import React, { useState, useContext, useEffect, useCallback } from 'react'
 import { BrowserRouter, Route, Link, Redirect } from 'react-router-dom'
-import { MyPageDisplayInfo } from './MyPage';
-import { LoginPage } from './LoginPage';
 import { UserList } from './UserList';
 import { CheckIn } from './CheckIn';
 import { ShopInfo } from './ShopInfo';
@@ -9,17 +7,26 @@ import { CustomInfo } from './CustomInfo';
 import { BbsMessageTop } from './BbsMessage';
 import { BbsThreadTop } from './BbsThread';
 import { AuthProvider , AuthContext } from './Auth';
+import { MyPageDisplayInfo } from './MyPage'
+import { LoginPage } from './LoginPage'
 
 export default () => {
     return (
         <AuthProvider>
             <BrowserRouter>
-                <div>
-                    {/*    <Route exact path="/" render={()=> {
-                        auth.currentUser ? <Redirect to ="/dashboard" /> : <Home />
-    )}}/> */}
+                <LoginRoute />
+            </BrowserRouter>
+        </AuthProvider>
+    );
+}
+
+const LoginRoute = () => {
+    const auth = useContext(AuthContext)
+
+    if ( auth.currentUser == true ){
+        return (
+            <div>
                 <Route exact path="/" component={Home} />
-                <Route exact path="/login" component={LoginPage} />
                 <Route path="/checkin/:id" component={CheckIn} />
                 <Route path="/userlist" component={UserList} />
                 <Route path="/bbsthread" component={BbsThreadTop} />
@@ -28,17 +35,22 @@ export default () => {
                 <Route path="/custominfo/:id" component={CustomInfo} />
                 <Route path="/mypage" component={MyPageDisplayInfo} /> 
             </div>
-        </BrowserRouter>
-    </AuthProvider>
-    );
+        );
+    } else {
+        return (
+            <div>
+                <Route path="/" component={LoginPage} />
+            </div>
+        )
+    }
 }
 
 const Home = () => { 
     const auth = useContext(AuthContext)
     return (
-    <div>
-        { auth.currentUser ? "hoge" : "fuga" }
-    </div>
+        <div>
+            { auth.currentUser ? "hoge" : "fuga" }
+        </div>
     );
 }
 
