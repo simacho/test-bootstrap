f03e1c7e94f3c723c9ea0a62ffd05d0ebdc7f02d
Katsuya Shimazaki <simazakikatuya@gmail.com>
2019-07-06 18:04:23 +0900

    change

--- src/components/Vertex.js
+++ src/components/Vertex.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
-import { useState , useEffect ,useCallback , useMemo } from "react";
-import { Button , Alert , Badge , Form } from 'react-bootstrap';
+import { createContext, useState , useEffect ,useCallback , useMemo } from "react";
+import { Button , Alert , Badge , Form , ListGroup } from 'react-bootstrap';
 import firebase from 'firebase';
 import firebaseui from 'firebaseui';
 import { firebaseDb } from '../firebase/index.js'
 import * as util from './Util.js';
+
+const VertexContext = createContext()
 
 const VertexCreate = (props) => {
     const [ name , setName ] = useState( "" )
@@ -40,21 +42,59 @@ const VertexCreate = (props) => {
     );
 }
 
-// ノード毎の処理
-const Vertex = (props) => {
+
+
+//  ノードツリー部分の描画処理
+const VertexDisp = (props) => {
+
+    const toggle = () => {
+        if ( props.vtx.collapse === null ) props.vtx.collapse = true
+        props.vtx.collapse = !props.vtx.collapse
+        console.log("toggle called " + props.vtx.collapse)
+    }
+
+
+    return (
+        <ListGroup.Item action onClick={toggle}>
+            {props.vtx.collapse ? 
+                <i class="fas fa-plus-square"></i> : <i class="fas fa-minus-square"></i>  }
+            {"name" in props.vtx ? props.vtx.name : ""}
+        </ListGroup.Item>
+        // collapse button
+        // information name
+    )
+    /*
+    return (
+    <tr>
+        <td><i class="fas fa-plus-square"></i></td>
+        <td>
+            {"name" in props.vtx ? props.vtx.name : ""}
+        </td>
+        <td><i class="fas fa-edit"></i><i class="fas fa-trash-alt"></i></td>
+        // collapse button
+        // information name
+    </tr>
+    )
+    */
+}
+
+// ノードツリー
+const VertexTree = (props) => {
     let adrs = props.crnt + '/' + props.vtx.name
+    let do_son = false;
+
+    if ( "son" in props.vtx && !("collapse" in props.vtx)) do_son = true;
+
     console.log(props)
     return (
         <div>
-            <tr>
-                <td>{props.crnt}</td>
-                <td>{"name" in props.vtx ? props.vtx.name : ""}</td>
-            </tr>
+            <ListGroup as="ul">
+                <VertexDisp vtx={props.vtx} />
+            </ListGroup>
             {
-                
-                "son" in props.vtx ? 
+                do_son ? 
                     Object.keys(props.vtx.son).map( sn => {
-                        return <Vertex crnt={props.crnt} vtx={props.vtx.son[sn]} />
+                        return <VertexTree crnt={props.crnt} vtx={props.vtx.son[sn]} />
                     }) : ""
             }
         </div>
@@ -66,6 +106,7 @@ export const VertexRoot = (props) => {
     const [ name , setName ] = useState( "" )
     const [ address , setAddress ] = useState( props.match.params.address )
     const [ vtx , setVtx ] = useState( {} )
+    const [ crnt , setCrnt ] = useState( "" )
 
     // ノード情報の読み込み
     useEffect(() => {
@@ -84,12 +125,14 @@ export const VertexRoot = (props) => {
 
     return (
         <div>
-            <table class="table">
-                {
-                    'vtx0' in vtx ? <Vertex crnt='name/vtx0' vtx={vtx['vtx0']} /> :
-                        "Nothing Root Vertex"
-                }
-            </table>
+            <VertexContext.Provider value={{ crnt }}>
+                <table class="table">
+                    {
+                        'vtx0' in vtx ? <VertexTree crnt='name/vtx0' vtx={vtx['vtx0']} /> :
+                            "Nothing Root Vertex"
+                    }
+                </table>
+            </VertexContext.Provider>
         </div>
     )
 
