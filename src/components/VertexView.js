b54ddf77d57abe3999d8f397ec9b665158c2b852
Katsuya Shimazaki <simazakikatuya@gmail.com>
2019-09-02 22:41:03 +0900

    change

--- src/components/VertexView.js
+++ src/components/VertexView.js
@@ -1,22 +1,25 @@
 import React, { Component } from 'react';
 import { useContext, createContext, useState , useEffect ,useCallback , useMemo } from "react";
-import { Button , Alert , Badge , Form , ListGroup } from 'react-bootstrap';
+import { Button , Alert , Badge , Form , FormControl , ListGroup } from 'react-bootstrap';
+import {  Dropdown, DropdownButton , ButtonGroup } from 'react-bootstrap';
+import {  Modal , InputGroup } from 'react-bootstrap';
 import { Sentence , SentenceContext } from './Sentence';
 import { VertexProvider , VertexContext , VertexCreate } from './Vertex'
 
 import * as util from './Util.js';
 
-const isValueInfo = (val) => { return val.match(/[_].+/) }
+const isValueInfo = (val) => { return val.match(/^[_].+/) }
 
 // ノードツリー
 const VertexTree = (props) => {
     const vc = useContext(VertexContext)
     let depth = parseInt(props.depth)
     const [collapse , setCollapse] = useState( false )
+    const [dlg , setDlg] = useState(0)
 
     // Object かつ _で始まる要素を抜いた物を子供とする
-    var list = Object.keys(props.vtx).filter( x => util.isObject(props.vtx[x]))
-    var list2 = list.filter( x => !isValueInfo(x) )
+    var list = Object.keys(props.vtx).filter(
+        x => util.isObject(props.vtx[x]) && !isValueInfo(x))
 
     // ボタンでの開閉処理
     const VertexCollpaseButton = () => {
@@ -27,19 +30,64 @@ const VertexTree = (props) => {
         )
     }
 
+    const VertexModalEdit = () => {
+        const isDlgEdit = () => { return dlg == 1 ? true : false }
+
+        return (
+            <Modal show={isDlgEdit()} onHide = {setDlg}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Edit</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <InputGroup size="sm" className="mb-3">
+                        <InputGroup.Prepend>
+                            <InputGroup.Text id="inputGroup-sizing-sm">Small</InputGroup.Text>
+                        </InputGroup.Prepend>
+                        <FormControl aria-label="Small" aria-describedby="inputGroup-sizing-sm" />
+                    </InputGroup>
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary">
+                        Close
+                    </Button>
+                    <Button variant="primary">
+                        Save Changes
+                    </Button>
+                </Modal.Footer>
+            </Modal>
+        );
+    }
+
+    // ドロップダウンメニュー
+    const VetexDropDown = () => {
+        return (
+            <div class="float-right">
+            <ButtonGroup>
+                <DropdownButton variant="secondary" size="sm" id="dropdown-item-button" title="" >
+                    <Dropdown.Item as="button" onSelect={()=> setDlg(1) } >Edit</Dropdown.Item>
+                    <Dropdown.Item as="button">Delete</Dropdown.Item>
+                    <Dropdown.Item as="button">Cleate</Dropdown.Item>
+                </DropdownButton>
+            </ButtonGroup>
+        </div>
+        )
+    }
+
     const VertexDisp = () => {
 
         return (
             <ListGroup.Item
                 action onClick={vc.setcrnt}
                 active = {vc.crnt == props.key ? true : false }
-            > {'---'.repeat(depth)}
-                {"_fullname" in props.vtx ? props.vtx._fullname : ""}
-                {list2.length >=1 && <VertexCollpaseButton />}
+            >
+                {'---'.repeat(depth)}
+                {"_name" in props.vtx ? props.vtx._name : ""}
+                {list.length >=1 && <VertexCollpaseButton />}
+                <VetexDropDown />
             </ListGroup.Item>
         )
     }
-    // 
+
     // 
     return (
         <div>
@@ -48,10 +96,11 @@ const VertexTree = (props) => {
             </ListGroup>
             {
                 collapse ? "" : 
-                list2.map( sn => {
+                list.map( sn => {
                     return <VertexTree key={sn} vtx={props.vtx[sn]} depth={depth+1} />
                 })
             }
+            <VertexModalEdit />
         </div>
     ) 
 }
